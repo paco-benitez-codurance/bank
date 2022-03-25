7aa64d082ea5edd529a88eefa02e8009d6b410ff
Rita Teixeira <rita.teixeira@codurance.com>
2022-03-25 17:43:51 +0100

    second test

--- app/src/main/java/bank/AccountServiceImpl.java
+++ app/src/main/java/bank/AccountServiceImpl.java
@@ -2,24 +2,28 @@ package bank;
 
 public class AccountServiceImpl implements AccountService {
 
-    private static final String HEADER = "Date || Amount || Balance\n";
-    private final Console console;
+  private static final String HEADER = "Date || Amount || Balance\n";
+  private final Console console;
+  private boolean hasDeposit;
 
-    public AccountServiceImpl(Console console) {
-        this.console = console;
-    }
+  public AccountServiceImpl(Console console) {
+    this.console = console;
+  }
 
-    @Override
-    public void deposit(int amount) {
-    }
+  @Override
+  public void deposit(int amount) {
+    hasDeposit = true;
+  }
 
-    @Override
-    public void withdraw(int amount) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void withdraw(int amount) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public void printStatement() {
-        console.writeInConsole(HEADER);
-    }
+  @Override
+  public void printStatement() {
+    if (hasDeposit) {
+      console.writeInConsole(HEADER + "10/01/2012 || 1000 || 1000");
+    } else console.writeInConsole(HEADER);
+  }
 }
--- app/src/test/java/bank/AccountServiceShould.java
+++ app/src/test/java/bank/AccountServiceShould.java
@@ -6,29 +6,30 @@ And a deposit of 2000 on 13-01-2012
 And a withdrawal of 500 on 14-01-2012
 When they print their bank statement
  */
-import org.junit.jupiter.api.Disabled;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class AccountServiceShould {
 
-  @Captor
-  ArgumentCaptor<String> stringArgumentCaptor;
+  AccountService accountService;
+  Console console;
+
+  @BeforeEach
+  void setup() {
+    console = mock(Console.class);
+    accountService = new AccountServiceImpl(console);
+  }
 
   @Test
   void when_print_statement_console_writeInConsoleIsCalled() {
-    Console console = mock(Console.class);
-
     String expectedOutput = "Date || Amount || Balance\n";
-
-    AccountService accountService = new AccountServiceImpl(console);
     accountService.printStatement();
 
     verify(console).writeInConsole(expectedOutput);
@@ -36,15 +37,10 @@ public class AccountServiceShould {
 
   @Test
   void a_client_can_make_a_deposit() {
-    Console console = mock(Console.class);
-
-    AccountService accountService = new AccountServiceImpl(console);
     accountService.deposit(1000);
-    String expectedOutput = "Date       || Amount || Balance\n" + "10/01/2012 || 1000   || 1000";
+    String expectedOutput = "Date || Amount || Balance\n" + "10/01/2012 || 1000 || 1000";
     accountService.printStatement();
 
-    verify(console).writeInConsole(stringArgumentCaptor.capture());
-
-    assertTrue(stringArgumentCaptor.getValue().contains("1000"));
+    verify(console).writeInConsole(expectedOutput);
   }
 }
